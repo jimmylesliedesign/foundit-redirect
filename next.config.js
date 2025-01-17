9628c2bf18a95ed6f222a2989a1b37a56655edac
jimmylesliedesign <65221442+jimmylesliedesign@users.noreply.github.com>
2025-01-17 22:34:00 +0700

    Update next.config.js to vercel.json

--- next.config.js
+++ next.config.js
@@ -1,18 +1,9 @@
-module.exports = {
-  // Add any Next.js configuration options here
-  // e.g., redirects, rewrites, etc.
-  env: {
-    // Expose environment variables to the client-side
-    AIRTABLE_BASE_ID: process.env.AIRTABLE_BASE_ID,
-    AIRTABLE_TOKEN: process.env.AIRTABLE_TOKEN,
-  },
-  // Vercel-specific configuration
-  rewrites: async () => {
-    return [
-      {
-        source: '/:tagId',
-        destination: '/api/[tagId]',
-      },
-    ];
-  },
-};
+{
+  "version": 2,
+  "routes": [
+    {
+      "src": "/(.*)",
+      "dest": "/api/redirect.js"
+    }
+  ]
+}
